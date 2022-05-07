ee8cda35d6c5d7031b0b378c533ba158e1a8e877
Boyan Sun <bsun68@wisc.edu>
2022-05-06 21:38:40 -0500

    实验登陆接口

--- cssa-mini/pages/activity/act.vue
+++ cssa-mini/pages/activity/act.vue
@@ -1,12 +1,54 @@
 <template>
-	<view><div>a</div></view>
+	<view>
+		<button @click="getUserProfile">授权登陆</button>
+		<div class="avatar-box"><img class="avatar" :src="userInfo.avatarUrl"></div>
+		
+		<div>a</div>
+		
+	</view>
 </template>
 
 <script>
-export default{
-	
-}
+	export default {
+		data(){
+			return{
+				userInfo:{},
+			}
+		},
+		mounted() {
+
+		},
+		methods: {
+			getUserProfile: function() {
+				if (uni.getUserProfile) {
+					uni.getUserProfile({
+						desc: "获取用户头像",
+						success: (userProfile) => {
+							this.userInfo = userProfile.userInfo;
+							console.log(this.userInfo);
+						},
+						fail: (msg) => {
+							console.log(msg);
+						}
+					});
+				}
+			}
+		}
+	}
 </script>
 
 <style>
+	.avatar-box{
+		width:100vw;
+		height: 50px;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+	}
+	.avatar{
+		width:50px;
+		height: 50px;
+		border-radius: 100%;
+	}
 </style>
