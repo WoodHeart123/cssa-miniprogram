662205f7ac6ad74367ed3bde599fe5ae53172afe
haoduo_sun <accounts_62816a30588d6412eb1024f2@mail.teambition.com>
2022-05-22 05:56:21 +0800

    上传 finished.vue

---
+++ cssa-mini/pages/activity/finished.vue
@@ -0,0 +1,43 @@
+<template>
+    <view>
+
+
+        <!-- a BIG title indicating YOU FINISHED -->
+        <view class="finished-title">
+            <text>您已完成活动注册</text>
+        </view>
+
+        <!-- A BUTTON REDIRECT TO HOME PAGE -->
+        <view class="finished-button">
+            <button @click="jumpToHomePage">返回活动首页</button>
+        </view>
+
+
+
+
+    </view>
+</template>
+
+<script>
+    export default {
+        methods: {
+            jumpToHomePage() {
+                wx.switchTab({
+                    url: '/pages/activity/act'
+                });
+            }
+        }
+    }
+                
+</script>
+
+<style>
+.finished-title {
+    width: 100vw;
+    height: 100px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+}
+</style>
