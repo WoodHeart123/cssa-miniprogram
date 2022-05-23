75eb1ded6de2888bfa5be28eab47783540d6bf39
Boyan Sun <bsun68@wisc.edu>
2022-05-23 12:05:27 -0500

    修改备注页

--- cssa-mini/pages/activity/remark.vue
+++ cssa-mini/pages/activity/remark.vue
@@ -1,141 +1,104 @@
 <template>
-	<view>
-
-		<!-- description of activity here -->
-		<!-- note section and store the string in this.text -->
-		<p>输入你的备注</p>
-		<view class="content">
-			<view class="input">
-				<input type="text" v-model="text" bindchange="updateInputBox" placeholder="请输入内容" />
-			</view>
+	<view id="remark">
+		<view class="line" v-for="(info,index) in actDetail.additionalInfo" :key="index">
+			<view class="name"><text>{{info.name}}</text></view>
+			<input class="input" v-if="info.type=='input'" type="text" maxlength="10" placeholder="请输入内容" @focus="changeIndex(index)" @input="bindChange"/>
+			<picker class="picker" v-if="info.type=='select'" mode="selector" :range="info.options" :value="info.value" @focus="changeIndex(index)" @change="bindChange">
+				<view class="input-text">{{info.options[info.value]}}</view>
+			</picker>
 		</view>
-
-		<!-- multiple choice section -->
-
-		<p>选择你的学院</p>
-		<picker mode='selector' bindchange="bindPickerChange" value="{{index}}" range="{{array}} " range-key="name">
-			<view class="picker">
-				当前选择：{{ array[index].name }}
-			</view>
-		</picker>
-
-
-
-		<view>
-			<button @click="postBotton">完成</button>
+		<view >
+			<uni-goods-nav :buttonGroup="buttonGroup" :options="options" fill="true" @buttonClick="submit"></uni-goods-nav>
 		</view>
-
-
 	</view>
-
-
 </template>
 
 <script>
-export default {
-	data() {
-		return {
-			userInfo: {},
-			activityInfo: {},
+	export default {
+		data() {
+			return {
+				userInfo: {},
+				buttonGroup: [{
+					text: '提交',
+					backgroundColor: "#1684FC",
+					color: '#fff'
+				}],
+				actDetail:{},
+				options:[],
+				index:0,
+				}
+		},
+		onLoad(options) {
+			this.actDetail = JSON.parse(decodeURIComponent(options.actDetail));
+		},
+		methods: {
+			jumpToHomePage() {
+				wx.switchTab({
+					url: '/pages/activity/act'
+				});
+			},
+			changeIndex:function(i){
+				this.index = i;
+			},
+			bindChange: function(e) {
+				this.actDetail.additionalInfo[this.index].value = e.detail.value;
+			},
+			submit:function() {
+				console.log(this.actDetail);
+				// wx.navigateTo({
+				// 	url: '/pages/activity/finished'
+				// });
 
-			//array: ['赫奇帕奇', '格兰芬多', '拉文克劳', '斯莱特林'],
-			array: [{ name: '赫奇帕奇' }, { name: '格兰芬多' }, { name: '拉文克劳' }, { name: '斯莱特林' }],
-			index: 0,
+			},
+
 
 		}
-	},
-	mounted() {
-
-	},
-	onload() {
-		_self = this;
-		//this.getUserProfile();
-		//this.getActivityInfo();
-	},
-	methods: {
-		jumpToHomePage() {
-			wx.switchTab({
-				url: '/pages/activity/act'
-			});
-		},
-
-		bindPickerChange: function (e) {
-			console.log('picker发送选择改变，携带值为', e.detail.value)
-			this.setData({
-				index: e.detail.value
-			})
-		},
-		updateInputBox: function (e) {//not actually used
-			console.log('input发送改变，携带值为', e.detail.value)
-			this.setData({
-				remarkInfo: e.detail.value
-			})
-		},
-		postBotton() {
-			if (this.text == '' || this.text == null || this.text == undefined) {
-				wx.showToast({
-					title: '请输入内容',
-					icon: 'none',
-					duration: 2000
-				})
-				return;
-			}
-			if (this.index == -1) {
-				wx.showToast({
-					title: '请选择学院',
-					icon: 'none',
-					duration: 2000
-				})
-				return;
-			}
-
-			console.log(this.text);
-			console.log(this.array[this.index].name);
-			//TODO: pass this.text to server
-
-			//TODO: pass this.array[index] to server
-
-			//TODO: end the process, e.g. jump to finish page
-			wx.navigateTo({
-				url: '/pages/activity/finished'
-			});
-
-			setTimeout(function (e) {
-				_self.text = '';
-			}, 1500);
-
-		},
-
-
 	}
-}
 </script>
 
 <style>
+	#remark{
+		width:100vw;
+		height:100vh;
+	}
+	.picker {
+		width: 80%;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		font-size:15px;
+		padding-bottom: 5px;
+		border-bottom: 1px solid #eee;
+	}
 
 
-.picker {
-	width: 100vw;
-	height: 50px;
-	display: flex;
-	flex-direction: row;
-	align-items: center;
-	justify-content: center;
-}
+	.content {
+		padding: 17upx 34upx;
+	}
 
+	.input {
+		width: 80%;
+		border-bottom: 1px solid #eee;
+	}
 
-.content {
-	padding: 17upx 34upx;
-}
-
-.input {
-	width: 100%;
-	border: 1upx solid #eee;
-	margin-bottom: 20upx;
-	padding: 17upx 0;
-}
-
-input {
-	padding-left: 20upx;
-}
+	.line {
+		margin-top: 10px;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		width: 100%;
+		height: 50px;
+		padding: 10px 0 10px 0;
+		background-color: white;
+	}
+	.name{
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+		width:20%;
+	}
+	.input-text{
+		
+	}
 </style>
