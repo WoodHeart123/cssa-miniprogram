aa173e2da3006dd649a5ece75749d32be25a7f2b
Boyan Sun <bsun68@wisc.edu>
2022-05-24 10:06:24 -0500

    后端

---
+++ cssa/src/main/java/com/example/cssa/CssaApplication.java
@@ -0,0 +1,13 @@
+package com.example.cssa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CssaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CssaApplication.class, args);
+    }
+
+}
---
+++ cssa/src/test/java/com/example/cssa/CssaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cssa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CssaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
