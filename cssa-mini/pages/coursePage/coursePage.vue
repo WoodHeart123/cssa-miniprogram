1f3893c15686bf2fd711336b8feccecb40b1b0dc
Boyan Sun <bsun68@wisc.edu>
2022-09-24 15:55:36 -0500

    完成课程界面上半部分

--- cssa-mini/pages/coursePage/coursePage.vue
+++ cssa-mini/pages/coursePage/coursePage.vue
@@ -2,26 +2,24 @@
 	<uni-transition ref="course" :show=true customClass="full-screen">
 		<view class="column-container">
 			<view class="course-name">
-				<view comment="courseName"><Text>Biophysical Spectroscopy Biophysical Spectroscopy</Text></view>
+				<view comment="courseName"><Text>Biophysical Spectroscopy Biophysical Spectr oscopy Biophysical Spectroscopy Biophysical Spectr oscopy</Text></view>
 			</view>
-			<view class="row-container" style="-webkit-justify-content: space-between;justify-content: space-between;">
+			<view class="row-container course-intro-box">
 				<view class="course-diff-like">
 					<view class="row-container rate-box">
-						<view><text>难度 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</text></view>
-						<uni-rate readonly="true" :value="4.5" allowHalf="true" size="15"></uni-rate>
-						<view><text>(4.5)</text></view>
+						<view class="rate-text"><text>难度:</text></view>
+						<uni-rate readonly="true" :value="4.5" allowHalf="true" size="20"></uni-rate>
+						<view class="rate-num"><text>4.5</text></view>
 					</view>
 					<view class="row-container rate-box">
-						<view><text>喜爱 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </text></view>
-						<uni-rate readonly="true" :value="1" allowHalf="true" size="15"></uni-rate>
-						<view><text>(1)</text></view>
+						<view class="rate-text"><text>喜爱:</text></view>
+						<uni-rate readonly="true" :value="1" allowHalf="true" size="20"></uni-rate>
+						<view class="rate-num"><text>1</text></view>
 					</view>
 				</view>
-				<view class="credit">
-					<text style="font-size: 30pt; text-align: center; vertical-align: 10pt;">2-3</text>
-					<view>
-						<text style="font-size: 10pt; text-align: right;">学分</text>
-					</view>
+				<view class="column-container credit-box">
+					<text class="credit">2-3</text>
+					<text style="font-size: 10pt; text-align: center;">学分</text>
 				</view>
 			</view>
 			<view class="footnote">56人参与讨论</view>
@@ -86,7 +84,14 @@
 		margin: 5px 0px 5px 5px;
 	}
 
+	.course-intro-box{
+		width: 100%;
+		height: 80px;
+		justify-content: space-between;
+		align-items: center;
+	}
 	.course-name {
+		height: 40px;
 		margin-top: 5px;
 		margin-bottom: 5px;
 		width: calc(100% - 20px);
@@ -94,24 +99,38 @@
 		margin: 10px 10px 10px 10px;
 		text-align: left;
 		line-height: 20px;
+		overflow: hidden;
+		white-space: pre-wrap;
+		text-overflow: ellipsis;
+		display: -webkit-box;
+		-webkit-line-clamp: 2;
+		-webkit-box-orient: vertical;
 	}
 
 	.course-diff-like {
 		font-weight: 250;
 	}
 
-	.credit {
+	.credit-box{
+		width: 70px;
+		height: 100%;
 		background-color: #cbcbcb;
 		border-radius: 10px 0 0 10px;
 		color: white;
+		justify-content: center;
 	}
 
+	.credit{
+		font-size: 30px;
+		text-align: center;
+	}
 
 	.footnote {
+		
 		color: #aaa;
 		font-size: 10px;
 		text-align: right;
-		margin-right: 10px;
+		margin-right: 2px;
 	}
 
 	.filter-selected {
@@ -126,12 +145,24 @@
 		border-radius: 20px;
 		text-align: center;
 		margin-left: 10px;
-		background-color: #F5F5F5;
+		background-color: white;
 		justify-content: space-around;
 		align-items: center;
 	}
 
 	.box {
 		margin-bottom: 20px;
+	}
+	.rate-box{
+		height: 20px;
+		margin-bottom: 10px;
+	}
+	.rate-text{
+		margin-right:10px;
+	}
+	.rate-num{
+		margin-left: 20px;
+		font-size: 12px;
+		line-height: 20px;
 	}
 </style>
