06331187ecb628b67190070eb56a61c3699644f1
haiyi <hwang866@wisc.edu>
2022-09-26 17:50:50 -0500

    first develop a simple sample

---
+++ cssa-mini/components/com-box/com-box.vue
@@ -0,0 +1,18 @@
+<template>
+	<view class="container">你好</view>
+</template>
+
+<script>
+</script>
+
+<style>
+	.container {
+		width: 94vw;
+		height: 20vh;
+		margin-left: 3vw;
+		margin-top: 20px;
+		border-radius: 5px;
+		box-shadow: rgba(0, 0, 0, 0.2) 0 0 5px;
+		background-color: white;
+	}
+</style>
---
+++ cssa-mini/pages/community/com-main.vue
@@ -0,0 +1,63 @@
+<template>
+	<view id="community">
+		<view class="search-bar">
+			<uni-search-bar cancelButton="none" v-model="searchValue" @focus="onFocus" @blur="onBlur">
+			</uni-search-bar>
+		</view>
+		<view class="column-container" v-if="searching">
+			<view></view>
+		</view>
+		<com-box-vue></com-box-vue>
+		<com-box-vue></com-box-vue>
+		<com-box-vue></com-box-vue>
+		<com-box-vue></com-box-vue>
+		<com-box-vue></com-box-vue>
+	</view>
+</template>
+
+<script>
+	export default {
+		components: {
+			comBoxVue
+		},
+		data(){
+			return{
+				searching: false
+			}
+		},
+		onload() {
+			uni.startPullDownRefresh();
+		},
+		
+		onPullDownRefresh(){
+			console.log("refresh");
+			setTimeout(function() {
+				uni.stopPullDownRefresh();
+			},1000);
+		},
+		
+		methods: {
+			onFocus: function() {
+				this.searching = true;
+				console.log(this.searching);
+			},
+			onBlur: function() {
+				this.searching = false;
+			},
+		}
+	}
+	import comBoxVue from "@/components/com-box/com-box.vue"
+</script>
+
+<style>
+	.search-bar {
+		height: 10vh;
+		background-color: white;
+	}
+	
+	.column-container {
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+	}
+</style>
