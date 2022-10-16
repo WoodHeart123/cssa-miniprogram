76084060ffbda66bcd20be03a52974aaeee57f94
Boyan Sun <bsun68@wisc.edu>
2022-10-16 17:51:51 -0500

    top bar for second hand

--- cssa-mini/pages/second/secondMain.vue
+++ cssa-mini/pages/second/secondMain.vue
@@ -1,11 +1,16 @@
 <template>
-	<!-- <view class="button">cssa</view> -->
-	<view class="container">
-		<view class="ant">
-			<span>我不知道我不知道我不知道我不知道我不知道我不知道我不知道我不知道我不知道</span>
-			
+	<view>
+		<view class="menu row-container">
+			<view class="search-box">
+				<uni-icons type="search" size="30"></uni-icons>
+			</view>
+			<view class="menu-box row-container">
+				<view class="row-container product-type-item" v-for="(productType, index) in productTypeList" :key="index" @click="onClickMenu(index)">
+					<text :class="currentIndex==index?'product-text selected':'product-text'">{{productType.name}}</text>
+				</view>
+			</view>
 		</view>
-		
+		<view></view>
 	</view>
 </template>
 
@@ -13,42 +18,73 @@
 	export default {
 		data() {
 			return {
-				
+				productTypeList: productTypeList, 
+				currentIndex:0,
 			}
 		},
 		methods: {
-			
+			onClickMenu:function(index){
+				this.currentIndex = index;
+			},
 		}
 	}
+	import productTypeList from './secondMain.js'
 </script>
 
 <style>
-	
-.container{
-	display: flex;
-	height: 500px;
-	width: 80vw;
+	.column-container {
+		display: flex;
+	}
 
-}
-.ant{
-	line-height: 22px;
-	font-size: 15px;
-	overflow: hidden;
-	overflow-x: hidden;
-	/* white-space: pre-wrap;	 */
-	text-overflow: ellipsis;
-	/* display: -webkit-box; */
-	/* -webkit-line-clamp: 4; */
-	/* -webkit-box-orient: vertical; */
-	width: 100px;
-	max-height: 110px;
-	/* min-height: 50px; */
-}
-.button{
-	position: fixed; 
-	border-radius: 20px;
-	width: 100px;
-	height: 50px;
-	color:white;
-}
+	.row-container {
+		display: flex;
+		flex-direction: row;
+	}
+
+	.menu {
+		position: fixed;
+		width: 100vw;
+		height: 50px;
+		align-items: center;
+		background-color: white;
+		padding-bottom: 10px;
+		transition: all 1s;
+	}
+	.menu-scroll{
+		padding-bottom: 0;
+	}
+	.search-box {
+		margin-left: 5px;
+		width: 30px;
+	}
+	.menu-box{
+		width: calc(100vw - 35px);
+		overflow-x: scroll;
+		height: 50px;
+		-ms-overflow-style: none;
+		scrollbar-width: none;
+		flex-shrink: 0;
+	}
+	.product-type-item{
+		margin-left: 10px;
+		height: 100%;
+		align-items: center;
+		justify-content: center;
+		width: fit-content;
+		flex-shrink: 0;
+	}
+	.product-text{
+		font-size: 15px;
+		color:#aaa;
+		margin-right: 10px;
+	}
+	.selected{
+		font-size: 18px;
+		font-weight: 700;
+		color:black;
+	}
+	
+	.menu-box::-webkit-scrollbar {
+	  display: none;
+	}
 </style>
