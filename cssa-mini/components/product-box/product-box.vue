c2010151943ead56701665ad44f67cec0f7e1641
Ye <alinaliuye@outlook.com>
2022-10-16 15:22:13 -0500

    product-box appearance

---
+++ cssa-mini/components/product-box/product-box.vue
@@ -0,0 +1,89 @@
+<template>
+	<view class='product-box'>
+		<image class='photo' src="../../static/index/maomao.jpg" mode='aspectFill'></image>
+		<view class='product-name'>
+			<text>A cat 一只猫</text>
+		</view>
+		<view class='price-condition row-container'>
+			<view class='price'><text>$89</text></view> <!-- $+{{product.price}} -->
+			<view class='condition'><text>完好如初</text></view>
+		</view>
+		
+		<view class='seller row-container'>
+			<image class='avatar' src="../../static/index/maomao.jpg"></image>
+			<view class = 'seller-name'><text>路人甲jia</text></view>
+			<view class='time'><text>3小时前</text></view>
+		</view>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+	.row-container {
+		display: flex;
+		flex-direction: row;
+
+	}
+
+	.product-box {
+		background-color: white;
+		margin: 2vw;
+		width: 45vw;
+		height: 40vh;
+		border-radius: 10px;
+	}
+
+	.photo {
+		width: 100%;
+		height: 70%;
+		border-radius: 10px 10px 0px 0px;
+		overflow: hidden;
+	}
+
+	.product-name {
+		height: 10%;
+		font-weight: 600;
+		margin-left: 5%;
+		margin-right: 5%;
+	}
+
+	.price-condition {
+		justify-content: space-between;
+		height: 10%;
+		margin-left: 5%;
+		margin-right: 5%;
+	}
+
+	.price {
+		font-weight: 600;
+	}
+
+	.time {
+		font-size: 50%;
+		text-align: right;
+		margin-left: auto;
+		padding-top: 5%;
+	}
+
+	.condition {
+
+	}
+
+	.seller {
+		margin-left: 5%;
+		margin-right: 5%;
+		height: 10%;
+		font-size: 10pt;		
+	}
+	.avatar {
+		width: 20px;
+		height: 20px;/*??*/
+		border-radius: 50%;
+	}
+	.seller-name{
+		margin-left: 5%;
+	}
+	
+</style>
--- cssa-mini/pages/second/secondMain.vue
+++ cssa-mini/pages/second/secondMain.vue
@@ -3,52 +3,84 @@
 	<view class="container">
 		<view class="ant">
 			<span>我不知道我不知道我不知道我不知道我不知道我不知道我不知道我不知道我不知道</span>
-			
+
 		</view>
-		
+
 	</view>
+	<scroll-view scroll-y="true" show-scrollbar="true" refresher-enabled="true"
+		class="column-container comment-container" @scrolltolower="">
+		<view class="box">
+			<productBoxVue></productBoxVue>
+			<productBoxVue></productBoxVue>
+			<productBoxVue></productBoxVue>
+			<productBoxVue></productBoxVue>
+			<productBoxVue></productBoxVue>
+			<productBoxVue></productBoxVue>
+			<productBoxVue></productBoxVue>
+			<productBoxVue></productBoxVue>
+			<productBoxVue></productBoxVue>
+			<productBoxVue></productBoxVue>
+			<productBoxVue></productBoxVue>
+		</view>
+		<uni-load-more status="more"></uni-load-more>
+	</scroll-view>
 </template>
 
 <script>
 	export default {
+		components: {
+			productBoxVue
+		},
 		data() {
 			return {
-				
+
 			}
 		},
 		methods: {
-			
+
 		}
 	}
+	import productBoxVue from '@/components/product-box/product-box.vue'
 </script>
 
 <style>
-	
-.container{
-	display: flex;
-	height: 500px;
-	width: 80vw;
+	.container {
+		display: flex;
+		height: 500px;
+		width: 80vw;
 
-}
-.ant{
-	line-height: 22px;
-	font-size: 15px;
-	overflow: hidden;
-	overflow-x: hidden;
-	/* white-space: pre-wrap;	 */
-	text-overflow: ellipsis;
-	/* display: -webkit-box; */
-	/* -webkit-line-clamp: 4; */
-	/* -webkit-box-orient: vertical; */
-	width: 100px;
-	max-height: 110px;
-	/* min-height: 50px; */
-}
-.button{
-	position: fixed; 
-	border-radius: 20px;
-	width: 100px;
-	height: 50px;
-	color:white;
-}
+	}
+
+	.ant {
+		line-height: 22px;
+		font-size: 15px;
+		overflow: hidden;
+		overflow-x: hidden;
+		/* white-space: pre-wrap;	 */
+		text-overflow: ellipsis;
+		/* display: -webkit-box; */
+		/* -webkit-line-clamp: 4; */
+		/* -webkit-box-orient: vertical; */
+		width: 100px;
+		max-height: 110px;
+		/* min-height: 50px; */
+	}
+
+	.button {
+		position: fixed;
+		border-radius: 20px;
+		width: 100px;
+		height: 50px;
+		color: white;
+	}
+
+	.box {
+		display: flex;
+		display: -webkit-flex;
+		justify-content: left;
+		flex-direction: row;
+		flex-wrap:wrap;
+		margin-left: 1vw;
+		margin-right: 1vw;
+	}
 </style>
