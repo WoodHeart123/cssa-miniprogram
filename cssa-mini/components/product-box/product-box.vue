130b3162d7e115da9db952e2459aaf59e4257544
Boyan Sun <bsun68@wisc.edu>
2022-10-29 20:14:17 -0500

    slight style change on product box:

--- cssa-mini/components/product-box/product-box.vue
+++ cssa-mini/components/product-box/product-box.vue
@@ -2,15 +2,15 @@
 	<view class='product-box' @click="toSecondDetail">
 		<image class='photo' src="../../static/index/maomao.jpg" mode='aspectFill'></image>
 		<view class='product-name'>
-			<text class='delivery'>送货上门</text>
+			<text class='delivery'>邮寄</text>
 			<text>&nbspA cat 一只猫一只猫一只猫</text>
 		</view>
 		<view style='height: 12%;'>
 			<text class='description'>一只会喵喵叫的猫。汪汪汪汪汪汪汪汪汪汪汪汪汪汪汪汪汪汪汪汪汪汪汪汪汪汪汪汪汪汪汪汪呱呱呱</text>
 		</view>
-		<view class='row-container' style='justify-content: space-between;height: 10%;'>
+		<view class='row-container' style='justify-content: space-between;height: 10%;align-items: center;'>
 			<text class = 'price'>$89.7</text>
-			<view class='condition'><text style='vertical-align: middle;'>完好如初</text></view>
+			<view class='condition'><text>完好如初</text></view>
 		</view>
 		<view class='seller row-container'>
 			<image class='avatar' src="../../static/index/maomao.jpg"></image>
@@ -47,7 +47,7 @@
 	}
 
 	.product-box {
-		background-color: white;
+		background-color: rgb(2540,250,250);
 		margin: 2vw;
 		width: 45vw;
 		height: 40vh;
@@ -72,18 +72,18 @@
 	
 	.description {
 
-		font-size: 80%;
+		font-size: 12px;
 		display: -webkit-box;
 		overflow: hidden;
 		text-overflow: ellipsis;
 		word-wrap: break-word;
 		-webkit-line-clamp: 2;
 		-webkit-box-orient: vertical; 
+		color:darkgrey;
 	}
 
 	.price {
 		font-weight: 600;
-		height: 10%;
 		color: #1E90FF;
 		font-size: 120%;
 	}
@@ -92,20 +92,20 @@
 		font-size: 70%;
 		text-align: right;
 		margin-left: auto;
-		padding-top: 4%;
 		color:darkgrey;
 	}
 
 	.condition {
 		margin-top: 3%;
-		height:50%;
+		height:20px;
+		line-height: 20px;
 		padding-left: 3%;
 		padding-right: 3%;
 		text-align: center;
-		font-size: 60%;
-		color:orangered;
+		font-size: 12px;
+		color:#1E90FF;
 		border-radius: 5px;
-		border-color: orangered;
+		border-color: #1E90FF;
 		border-style: solid;
 		border-width: 1pt;
 
@@ -114,7 +114,7 @@
 	.seller {
 		height: 10%;
 		font-size: 10pt;
-		vertical-align: middle;
+		align-items: center;
 
 	}
 	
@@ -132,8 +132,6 @@
 		width: 60%;
 		font-size: 80%;
 		color: darkgrey;
-		vertical-align: middle;
-		padding-top: 2%;
 	}
 	
 	.delivery {
@@ -143,8 +141,8 @@
 		padding-right: 3%;
 		padding-top: 1%;
 		padding-bottom: 1%;
-		border-radius: 5pt;
-		font-size: 80%;
+		border-radius: 5px;
+		font-size: 12px;
 	}
 	
 </style>
