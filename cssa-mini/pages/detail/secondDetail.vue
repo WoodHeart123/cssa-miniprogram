2018caaf84246df5ce5d43bc0010b4a41670d2fc
Ann X <71995504+AX1ao@users.noreply.github.com>
2022-11-05 15:25:03 -0500

    add secondMainPost.vue

--- cssa-mini/pages/detail/secondDetail.vue
+++ cssa-mini/pages/detail/secondDetail.vue
@@ -9,48 +9,41 @@
 		<view class="basic">
 			<view class="price-box">
 				<view class="iconfont icon">&#xe70b;</view>
-				<view class="price">1200</view>
+				<view class="price">12</view>
 				<img class="shoucang" src="/static/yishoucang.png" :style="left"/>
 			</view>
 			<view class="second_desc">
-				<view class="quantity-tag"><view class="quantity-content">几乎全新</view></view>
-				<view class="deliver-tag"><view class="deliver-content">邮寄</view></view>
+				<view class="quantity-tag"><view class="quantity-content">全新</view></view>
+				<view class="deliver-tag"><view class="deliver-content">自取</view></view>
+				<text class = "second_name">{{secondItem.name}}</text>
 			</view>
-			<view class = "second_name">{{secondItem.name}}</view>
 		</view>
 		<view class="blank_line"></view>
 		<view class="contact">
 			<view class="contact-box">
-				<!--
 				<view class="contact-tag">
 					<view class="contact-content">联系方式</view>
 				</view>
-				-->
-				<view class="avatar-box">
-					<img class="avatar" 
-						:src="'https://cssa-mini.oss-cn-shanghai.aliyuncs.com/cssa-mini-avatar/' + 1 + '.png'">
-				</view>
-				<text class="nickname" style="margin-left: 5px; margin-top: 4vw;">小红豆</text>
 				<view class="fuzhi-bt" :style="left_1">
-					<view class="fuzhi-string" style="font-size: 11px; margin-left: 2px; color: #999999">复制</view>
 					<img class="fuzhi-img" src="/static/fuzhi.png" @click="setClipboardData">
 				</view>
 			</view>
-			<view class="weixin" style="margin-left: 13px; margin-bottom: 2vw;">微信号：{{secondItem.contact[0]}}</view>
+			<view style="margin-left: 13px;">微信号：{{secondItem.contact[0]}}</view>
+			<view style="margin-left: 13px; margin-bottom: 10px;">手机号：{{secondItem.contact[1]}}</view>
 		</view>
 		<view class="blank_line"></view>
 		<view class="description">
-			<!--
 			<view class="tit">
 				<view class="tit-tag">
 					<view class="tit-content">详情介绍</view>
 				</view>
+				<!--
 				<button class="share" style="background-color: #7f7f7f;height: 7vw; width: 20vw; padding-left: 20vw;" open-type="share">
 					<img class="share-img" src="/static/fenxiang.png">
 				</button>
+				-->
 			</view>
-			-->
-			<!--<view class="blank_line_1"></view>-->
+			<view class="blank_line_1"></view>
 			<!--
 			<scroll-view class="scroll_page" scroll-y="true" :style="height">
 				<rich-text  class="content">{{secondItem.content}}</rich-text>
@@ -81,12 +74,12 @@
 				left:"",
 				left_1:"",
 				secondItem:{
-					name:"Macbook Pro 2022 1TB M2 非海南免税版",
+					name:"Macbook Pro",
 					imageList:["/static/renwu.jpeg", "/static/renwu.jpeg", "/static/renwu.jpeg"],
-					contact:["123456"],
+					contact:["123456", "123456789"],
 					types:["电子产品", "学习用品"],
-					quantity: "几乎全新",
-					content: "全新 M2 芯片现身，13 英寸 MacBook Pro 实力大涨。同样的紧凑设计之下，电池续航最长达 20 小时1，并以主动散热系统让强悍性能持续迸发。还有绚丽的视网膜显示屏、FaceTime 高清摄像头和录音棚级麦克风也全部到位，一台超机动的专业级笔记本电脑整装待发。"
+					quantity: "全新",
+					content: "恶魔方面则是更像《女神异闻录》一些每个人只能携带一个恶魔上场，如果在战斗中更换“仲魔”，需要使用前文提到的“指挥官技能”。只不过本作并不像《女神异闻录》或者《幻影异闻录FE#》一样，除了主角之外都有固定的恶魔（好吧，或者说 Persona、幻影）。 作者：游戏时光VGtime https://www.bilibili.com/read/cv18456093?from=category_0 出处：bilibili在《灵魂骇客2》中，“仲魔”类似于“装备”，除了使用恶魔的技能和共享属性弱点之外，恶魔还会给角色提供数值面板方面的增益。也就是说，游戏鼓励玩家合成高等级的恶魔，以此获取更高的数值增益。 作者：游戏时光VGtime https://www.bilibili.com/read/cv18456093?from=category_0 出处：bilibili在《灵魂骇客2》中，“仲魔”类似于“装备”，除了使用恶魔的技能和共享属性弱点之外，恶魔还会给角色提供数值面板方面的增益。也就是说，游戏鼓励玩家合成高等级的恶魔，以此获取更高的数值增益。 作者：游戏时光VGtime https://www.bilibili.com/read/cv18456093?from=category_0 出处：bilibili"
 				},
 				distance_1: 0,
 				distance_2: 0,
@@ -134,7 +127,7 @@
 				}
 			})
 			query.select('.price').boundingClientRect();
-			query.select('.nickname').boundingClientRect();
+			query.select('.contact-content').boundingClientRect();
 			query.exec((res) => {
 				this.distance_1 = res[0].right;
 				this.distance_2 = res[1].right;
@@ -149,7 +142,7 @@
 		methods: {
 			setClipboardData: function() {
 				uni.setClipboardData({
-					data: " 微信号: " + this.secondItem.contact[0],
+					data: "电话号: " + this.secondItem.contact[0] + " 微信号: " + this.secondItem.contact[1],
 					success: function () {
 						console.log('success');
 					}
@@ -245,22 +238,19 @@
 						border-radius: 5px;
 					}
 				}
-			}
-			.second_name {
-				padding-left: 13px;
-				padding-right: 13px;
-				padding-top: 5px;
-				padding-bottom: 5px;
-				font-size: 18px;
-				//line-height: 40px;
-				font-weight: 700;
+				.second_name {
+					padding-left: 10px;
+					padding-right: 10px;
+					font-size: 18px;
+					//line-height: 40px;
+					font-weight: 700;
+				}
 			}
 		}
 		.contact{
 			.contact-box{
 				display: flex;
 				flex-direction: row;
-				/*
 				.contact-tag{
 					margin-top: 7px;
 					font-size: 13px;
@@ -275,22 +265,12 @@
 						border-radius: 5px;
 					}
 				}
-				*/
-			   .avatar-box{
-				   margin-top: 1vw;
-				   margin-left: 13px;
-					.avatar{
-						height: 11vw;
-						width: 11vw;
-						border-radius: 50%;
-					}
-			   }
 				.fuzhi-bt{
 					height: 8vw;
 					width: 8vw;
 					//margin-left: 68vw;
 					//background-color: #db3024;
-					//margin-top: 1vw;
+					margin-top: 2vw;
 					.fuzhi-img{
 						height: 7vw;
 						width: 7vw;
@@ -376,7 +356,6 @@
 		.white{
 			//height: 9vh;
 			height: 2vh;
-			//style="font-size: 10px; margin-left: 88.7vw; color: "
 		}
 	}
 </style>
--- cssa-mini/pages/second/secondMainPost.vue
+++ cssa-mini/pages/second/secondMainPost.vue
@@ -4,48 +4,64 @@
 			<view class="image_upload">
 				<uni-file-picker limit="5"></uni-file-picker>
 			</view>
-		</uni-section>
-		<view class="uni-form-item uni-column">
-			<input class="uni-input" @blur="bindTextAreaBlur" maxlength="22" placeholder="请填写物品名称" placeholder-style="font-size:14px"/>
-		</view>
-		<view class="uni-textarea">
-			<textarea @blur="bindTextAreaBlur" placeholder="请输入商品内容信息" maxlength="400" placeholder-style="font-size:14px"/>
-		</view>
-		<view>
-			<label class="radio"><radio value="pickup" checked="true" style="transform:scale(0.8)"/>自取</label>
-			<label class="radio"><radio value="delivery" style="transform:scale(0.8)"/>可送货</label>
-			<label class="radio"><radio value="pnd" style="transform:scale(0.8)"/>都可以</label>
-		</view>
-		<view class="uni-list">
-			<view class="uni-list-cell">
-				<view class="uni-list-cell-left">
-					分类
+			<uni-group mode="card">
+				<view class="uni-form-item uni-column">
+					<input class="uni-input" @blur="bindTextAreaBlur" maxlength="22" placeholder="请填写商品名称" placeholder-style="font-size:14px"/>
 				</view>
-				<view class="uni-list-cell-db">
-					<picker @change="bindPickerChange" :value="index" :range="item_types">
-						<view class="uni-input">{{item_types[item_type_idx]}}</view>
-					</picker>
+			</uni-group>
+			<uni-group mode="card">
+				<view class="uni-textarea">
+					<textarea @blur="bindTextAreaBlur" placeholder="请输入商品内容信息" maxlength="400" placeholder-style="font-size:14px"/>
 				</view>
+			</uni-group>
+			<uni-group mode="card">
+				<view>
+					<label class="radio"><radio value="pickup" checked="true" color=#8B0000 style="transform:scale(0.8)"/>自取</label>
+					<label class="radio"><radio value="delivery" color=#8B0000 style="transform:scale(0.8)"/>可送货</label>
+					<label class="radio"><radio value="pnd" color=#8B0000 style="transform:scale(0.8)"/>都可以</label>
+				</view>
+			</uni-group>
+			<uni-group mode="card">
+				<view class="uni-list">
+					<view class="uni-list-cell">
+						<view class="uni-list-cell-left">
+							分类
+						</view>
+						<view class="uni-list-cell-db">
+							<picker @change="bindPickerChange" :value="index" :range="item_types">
+								<view class="uni-input">{{item_types[item_type_idx]}}</view>
+							</picker>
+						</view>
+					</view>
+				</view>
+			</uni-group>
+			<uni-group mode="card">
+				<view>
+					<label class="radio"><radio value="new" checked="true" color=#8B0000 style="transform:scale(0.8)"/>全新\t\t</label>
+					<label class="radio"><radio value="almostnew" color=#8B0000 style="transform:scale(0.8)"/>几乎全新\n</label>
+					<label class="radio"><radio value="used" color=#8B0000 style="transform:scale(0.8)"/>明显使用痕迹\t\t</label>
+					<label class="radio"><radio value="impaired" color=#8B0000 style="transform:scale(0.8)"/>部分损毁</label>
+				</view>
+			</uni-group>
+			<uni-group mode="card">
+				<view class="uni-form-item uni-column">
+					<input class="uni-input" @blur="bindTextAreaBlur" maxlength="10" placeholder="请填写价格 $"  placeholder-style="font-size:14px"/>
+				</view>
+			</uni-group>
+			<uni-group mode="card">
+				<view class="uni-form-item uni-column">
+					<input class="uni-input" @blur="bindTextAreaBlur" maxlength="22" placeholder="请填写联系方式(微信号)"  placeholder-style="font-size:14px"/>
+				</view>
+			<view>
+				<checkbox value="save_contact" checked="false" color=#8B0000 style="transform:scale(0.8);"/>保存联系方式，方便后续使用
 			</view>
-		</view>
-		<view>
-			<label class="radio"><radio value="pickup" checked="true" style="transform:scale(0.8)"/>全新</label>
-			<label class="radio"><radio value="delivery" style="transform:scale(0.8)"/>几乎全新</label>
-			<label class="radio"><radio value="pnd" style="transform:scale(0.8)"/>明显使用痕迹</label>
-			<label class="radio"><radio value="pnd" style="transform:scale(0.8)"/>部分损毁</label>
-		</view>
-		<view class="uni-form-item uni-column">
-			<input class="uni-input" @blur="bindTextAreaBlur" maxlength="10" placeholder="价格 $"  placeholder-style="font-size:14px"/>
-		</view>
-		<view class="uni-form-item uni-column">
-			<input class="uni-input" @blur="bindTextAreaBlur" maxlength="22" placeholder="联系方式(微信号)"  placeholder-style="font-size:14px"/>
-		</view>
-		<view>
-			<checkbox value="save_contact" checked="false" style="transform:scale(0.8)"/>保存联系方式，方便后续使用
-		</view>
-		<view class="uni-padding-wrap uni-common-mt">
-			<button type="default" plain="true">发布</button>
-		</view>
+			</uni-group>
+			<uni-group>
+				<view class="uni-padding-wrap uni-common-mt">
+					<button type="default" style="background-color: #8B0000; color: #ffffff;" plain="true">发布</button>
+				</view>
+			</uni-group>
+		</uni-section>
 	</view>
 </template>
 
@@ -86,14 +102,5 @@
 		padding: 14px;
 		padding-top: 0;
 	}
-	
-	.box {
-		display: flex;
-		display: -webkit-flex;
-		justify-content: left;
-		flex-direction: row;
-		flex-wrap: wrap;
-		margin-left: 1vw;
-	}
-	
+
 </style>
