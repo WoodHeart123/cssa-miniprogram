92685c5e7c8b5d49a038d58028bfb549b497fdf0
Boyan Sun <bsun68@wisc.edu>
2023-01-04 07:28:40 +0900

    create a template for user product box

---
+++ cssa-mini/components/user-product-box/user-product-box.vue
@@ -0,0 +1,27 @@
+<template>
+	<view id="user-prodcut-box">
+		<view class="title-box">
+			<text>{{product.title}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"user-product-box",
+		props:["product"],
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style>
+	#user-product-box{
+		width: 100%;
+		height: 100%;
+	}
+
+</style>
