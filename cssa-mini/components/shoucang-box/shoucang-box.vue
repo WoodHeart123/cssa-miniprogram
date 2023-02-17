a26c10616a526b310955ff7b051307dca8e916d4
haiyi <hwang866@wisc.edu>
2023-02-17 01:23:50 -0600

    filters and swipe delete

---
+++ cssa-mini/components/shoucang-box/shoucang-box.vue
@@ -0,0 +1,126 @@
+<template>
+	<view>
+		<uni-swipe-action>
+			<uni-swipe-action-item @click="bindClick">
+				<view style="padding: 10px; background-color: white;">
+					使用插槽
+				</view>
+				<template v-slot:right>
+					<view @click="bindClick()" class="slot-button"><text>删除</text></view>
+				</template>
+			</uni-swipe-action-item>
+		</uni-swipe-action>
+	</view>
+</template>
+
+<script>
+	export default {
+		props:['shoucangDetail'],
+		data(){
+			return{
+		
+			}
+		},
+		
+		methods:{
+			bindClick(){
+				this.$emit("delete", 1);
+				uni.showToast({
+					title: '已删除'
+				});
+			}
+		}
+		
+	}
+</script>
+
+<style>
+	.container {
+		width: 94vw;
+		margin-left: 3vw;
+		margin-top: 10px;
+		border-radius: 5px;
+		box-shadow: rgba(0, 0, 0, 0.2) 0 0 5px;
+		background-color: white;
+	}
+	
+	.slot-button {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+		padding: 0 20px;
+		background-color: #ff5a5f;
+	}
+	.top-container{
+		display: flex;
+		width: 50vw;
+		height: 17vw;
+	}
+	.image {
+		height: 13vw;
+		width: 13vw;
+		border-radius: 50%;
+		margin-left: 3vw;
+		margin-top: 3vw;
+	}
+	.user-name{
+		margin-left: 2.5vw;
+		font-size: 80%;
+		margin-top: 4vh;
+	}
+	.content-container{
+		margin-top: 1vh;
+	}
+	.title{
+		margin-left: 3vw;
+		margin-right:3vw;
+		font-weight: 450;
+	}
+	.content{
+		margin-top: 1vh;
+		margin-left: 3vw;
+		margin-right: 3vw;
+	}
+	.content-text{
+		font-size: 50%;
+		font-weight: 200;
+		line-height: 140%;
+	}
+	.image-box{
+		margin-top: 1vh;
+		margin-left: 3vw;
+		margin-right: 3vw;
+		display: flex;
+	}
+	.image-more{
+		display: flex;
+		flex-wrap: wrap;
+	}
+	.picture-more{
+		height: 15vh;
+		width: 25vw;
+		border-radius: 5%;
+		margin: 2px;
+	}
+	.picture-2{
+		height: 20vh;
+		width: 40vw;
+		border-radius: 5%;
+		margin: 5px;
+	}
+	.picture-1{
+		height: 20vh;
+		border-radius: 5%;
+		margin: 5px;
+	}
+	.whitespace{
+		height: 3vw;
+	}
+	.content-description{
+		font-size: 50%;
+		font-weight: 200;
+		line-height: 140%;
+		color: cornflowerblue;
+	}
+</style>
--- cssa-mini/pages/mySave/mySave.vue
+++ cssa-mini/pages/mySave/mySave.vue
@@ -1,22 +1,61 @@
 <template>
 	<view>
-		
+		<view class="row-container">
+			<view class="info-box"><text>全部</text></view>
+			<view class="info-box"><text>收藏</text></view>
+			<view class="info-box"><text>点赞</text></view>
+		</view>
+		<view class="boxes" v-for="(shoucangDetail,index) in shoucangDetailList">
+			<shoucang-box-vue v-on:delete="receive" :shoucangDetail="shoucangDetail"></shoucang-box-vue>
+		</view>
 	</view>
 </template>
 
 <script>
+	import shoucangBoxVue from "@/components/shoucang-box/shoucang-box.vue"
 	export default {
+		components:{
+			shoucangBoxVue
+		},
 		data() {
 			return {
-				
+				shoucangDetailList:[
+					{	
+						ID: 1,
+						avator: "https://cssa-mini.oss-cn-shanghai.aliyuncs.com/cssa-community-image/renwu.jpeg",
+						nickname:"Peter",
+						title: "Madison周围都有什么好玩的呢",
+						content: "Madison周围都有什么好玩的呢，有谁推荐一下吗，如果有推荐可以联系我：123456789. 大家可以一起玩，开心最重要了。",
+						shrinkContent: "",
+						imageUrls: ["https://cssa-mini.oss-cn-shanghai.aliyuncs.com/cssa-community-image/renwu.jpeg"],
+					}
+				]
 			}
 		},
 		methods: {
-			
+			receive: function(key1){
+				console.log(key1)
+				console.log(this.shoucangDetailList)
+				this.shoucangDetailList = this.shoucangDetailList.filter(user=> user.ID != key1)
+				console.log(this.shoucangDetailList)
+			}
 		}
 	}
 </script>
 
 <style>
-
+	.row-container {
+		display: flex;
+		flex-direction: row;
+	}
+	
+	.info-box{
+		padding-top: 10px;
+		padding-bottom: 10px;
+		width: 50vw;
+		text-align: center;
+		margin-bottom: 15px;
+		border: 1px solid #ACACAC;
+		background-color: rgba(255, 255, 255, 0.9);
+	}
 </style>
