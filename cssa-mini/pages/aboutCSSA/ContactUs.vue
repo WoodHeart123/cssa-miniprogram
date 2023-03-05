c48a1ea7401e60d8c8eee9f89030b23d34e14df5
XiaohanMei-max <xmei24@wisc.edu>
2023-03-05 16:31:49 -0600

    AboutCSSA Final

---
+++ cssa-mini/pages/aboutCSSA/ContactUs.vue
@@ -0,0 +1,77 @@
+<template>
+	<view>
+		<view class="topic-1">联系我们</view>
+		<view class = "gap"></view>
+		<view class="text-middle"> ->&nbsp&nbsp微信公众号: CSSAatUWMadison</view>
+		<view class = "gap"></view>
+		<view class="text-middle"> ->&nbsp&nbspB站账户: CSSAATUWMADISON</view>
+		<view class = "gap"></view>
+		<view class="text-middle"> ->&nbsp&nbsp新浪微博: @威斯康星大学UW-CSSA</view>
+		<view class = "gap"></view>
+		<view class="text-middle"> ->&nbsp&nbspYoutube: CSSA At UW-Madison</view>
+		<view class = "gap"></view>
+		<view class="text-middle"> ->&nbsp&nbspInstagram: Cssa_at-Uwmadison</view>
+		<view class = "gap"></view>
+		<view class="text-middle"> ->&nbsp&nbspInstagram: Facebook: CSSA At UW-Madison</view>
+		<view class="app_content_1">____________________________________________</view>
+		<view class="topic-2">&nbsp&nbsp&nbsp&nbsp新生微信群</view>
+		<view class = "small-gap"></view>
+		<view class="small-text">2023秋季新生群、家长群已建立，请添加下方学联小助手</view>
+		<view class="small-text">微信，并备注上自己的NetID，小助手会把你们拉进秋季新</view>
+		<view class="small-text">生群。家长也可以让小助手拉您进家长群,只需在加微信时</view>
+		<view class="small-text">备注上“家长”就可以了。</view>
+		<view class = "gap"></view>
+		<image class="image" src="../../static/QR.jpg"></image>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+	.topic-1 {
+		padding-top: 170rpx;
+		height: 450rpx;
+		text-align: center;
+		font-size: 60rpx;
+		font-weight: 400;
+		color: whitesmoke;
+		background-image: url('~@/static/ContactUs.jpeg');
+		background-size: cover;
+		background-repeat: no-repeat;
+	}
+	.topic-2 {
+		padding-top: 60rpx;
+		text-align: left;
+		font-size: 50rpx;
+		font-weight: 400;
+	}
+	.text-middle {
+		font-size: 35rpx;
+		font-weight: 300;
+		text-align:  center;
+	}
+	.gap {
+		height: 50rpx;
+	}
+	.small-gap {
+		height: 30rpx;
+	}
+	.app_content_1 {
+		padding-top: 0rpx;
+		text-align: center;
+		font-size:30rpx;
+		font-weight: 200;
+	}
+	.small-text {
+		font-size: 25rpx;
+		font-weight: 300;
+		text-align:  center;
+	}
+	.image {
+		height: 600rpx;
+		width: 500rpx;
+		display: block;
+		margin: 0 auto;
+	}
+</style>
