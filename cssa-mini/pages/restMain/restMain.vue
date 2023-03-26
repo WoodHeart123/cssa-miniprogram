dd3d31a8171ddaa53b9c84377e19fdfbcf8cbc35
ye <alinaliuye@outlook.com>
2023-03-26 18:36:01 -0500

    restMain started

---
+++ cssa-mini/pages/restMain/restMain.vue
@@ -0,0 +1,17 @@
+<template>
+	<view id="rest-main" class = "column-container">
+		
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+	@import '@/static/iconfont/iconfont.css';
+	
+	#rest-main {
+		width: 100vw;
+		height: 100vh;
+	}
+</style>
