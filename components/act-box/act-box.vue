98425c03579dccab6b7b99ac1ef6e4f1f6ce6f2e
Boyan Sun <bsun68@wisc.edu>
2024-04-11 16:15:33 -0500

    refactor act box UI

--- components/act-box/act-box.vue
+++ components/act-box/act-box.vue
@@ -1,26 +1,33 @@
 <template>
 	<view class="container" @click="toDetail">
+		<div class="float-box">
+			<div class="heading-3">
+				<text class="date">{{actDate}}</text>
+			</div>
+			<div class="paragraph-2">
+				<text class="month">{{actMonth}}</text>
+			</div>
+		</div>
 		<div class="box">
-			<div class="image"
-				:style="{'background-image': 'url(' + this.actDetail.images[0] +')' }" />
+			<div class="image" :style="{'background-image': 'url(' + this.actDetail.images[0] +')' }" />
 			<div class="lower-box">
 				<div class="content-box">
 					<div class="title heading-3">
-						<span>{{this.actDetail.title}}</span>
+						<text>{{this.actDetail.title}}</text>
 					</div>
 					<div class="row">
 						<image class="icon" src="@/static/act/time.svg"></image>
-						<span class="font-small paragraph-1">{{actDateFormat}}</span>
+						<text class="font-small paragraph-1">{{actDateFormat}}</text>
 					</div>
 					<div class="row">
 						<image class="icon" src="@/static/act/location.svg"></image>
-						<span class="font-small paragraph-1">{{this.actDetail.location}}</span>
+						<text class="font-small paragraph-1">{{this.actDetail.location}}</text>
 					</div>
-
 
 				</div>
 				<div class="button-box">
-					<view class="button" :class="{ 'disabled': ifJoined}"><text class="button-text">{{ifJoined?'已报名':'报名'}}</text></view>
+					<view class="button" :class="{ 'disabled': ifJoined}"><text
+							class="button-text">{{ifJoined?'已报名':'报名'}}</text></view>
 				</div>
 			</div>
 
@@ -39,8 +46,14 @@
 		},
 		computed: {
 			actDateFormat() {
-				return moment(this.actDetail.startDate).locale("cn").format("MM.DD/ddd H:mm - ") +
-				moment(this.actDetail.endDate).format("H:mm");
+				return moment(this.actDetail.startDate).locale("zh-cn").format("MM.DD/ddd H:mm - ") +
+					moment(this.actDetail.endDate).format("H:mm");
+			},
+			actMonth(){
+				return moment(this.actDetail.startDate).locale("zh-cn").format("MMMM")
+			},
+			actDate(){
+				return moment(this.actDetail.startDate).locale("zh-cn").format("DD")
 			}
 		},
 		methods: {
@@ -60,6 +73,7 @@
 	/* @import "@/static/iconfont.css"; */
 
 	.container {
+		position: relative;
 		width: 90vw;
 		margin-left: 5vw;
 		margin-top: 20px;
@@ -67,6 +81,33 @@
 		border-radius: 18px;
 		box-shadow: rgba(0, 0, 0, 0.2) 0 0 5px;
 		background-color: white;
+	}
+
+	.float-box {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		position: absolute;
+		top: 10px;
+		left: 10px;
+		background-color: white;
+		width: 50px;
+		height: 50px;
+		border-radius: 10px;
+
+	}
+
+	.date {
+		font-weight: 700;
+		line-height: 5.7px;
+		color: $main-primary-color !important;
+	}
+
+	.month {
+		font-weight: 500;
+		line-height: 13.7px;
+		color: $main-primary-color !important;
 	}
 
 	.box {
@@ -84,15 +125,15 @@
 			height: 100%;
 			padding-left: 7%;
 		}
-		
-		.button-box{
+
+		.button-box {
 			display: flex;
 			align-items: center;
 			justify-content: center;
 			min-height: 100%;
 			width: 50%;
-			
-			.button{
+
+			.button {
 				margin: 15px 0 0 30%;
 				width: 50%;
 				height: 40px;
@@ -150,5 +191,4 @@
 		border-radius: 100%;
 		margin-right: 5px;
 	}
-	
 </style>
