4a8e7d03ccb513df461959aaf2ebf17af991e6b8
Yizhe Shang <67483450+YizheShang@users.noreply.github.com>
2025-01-20 20:38:56 -0600

    更新了春晚文宣

--- pages/main/main.vue
+++ pages/main/main.vue
@@ -125,12 +125,12 @@
 				abortTask: false,
 				menuButtonInfo: {},
 				ads: [
+					{imgUrl: 'https://prod-9gip97mx4bfa32a3-1312104819.tcloudbaseapp.com/ads/main%20page%20ads/2025%E8%9B%87%E5%B9%B4%E6%98%A5%E6%99%9A%E6%97%B6%E9%97%B4%E5%9C%B0%E7%82%B9%E5%AE%98%E5%AE%A3.png?sign=b5f0583e5ad957c8c737eee90bf2d44a&t=1737426849',
+					 link: 'https://mp.weixin.qq.com/s/_GUT3WcbRnnSFtI4wn0-Dw',
+					 label: '2025蛇年春晚时间地点官宣'},
 					{imgUrl:'https://prod-9gip97mx4bfa32a3-1312104819.tcloudbaseapp.com/ads/main%20page%20ads/2025%E6%98%A5%E6%99%9A%E4%B8%BB%E6%8C%81%E4%BA%BA%E6%8B%9B%E5%8B%9F.png?sign=b9aa28b04686b9cb2dc41e85d3c001a9&t=1731120159',
 					 link: 'https://mp.weixin.qq.com/s/r21LNuaNVVmwSr09VOamcA',
-					 label: '2025蛇年导演组'},
-					{imgUrl: 'https://prod-9gip97mx4bfa32a3-1312104819.tcloudbaseapp.com/ads/main%20page%20ads/2025%E8%9B%87%E5%B9%B4%E5%AF%BC%E6%BC%94%E7%BB%84%E4%BB%8B%E7%BB%8D.png?sign=870111c1a696971f5c9934e321bd82a2&t=1733284485',
-					 link: 'https://mp.weixin.qq.com/s/Z1j-vIWHaEflqnAPmm1wVA',
-					 label: '2025蛇年春晚主持人招募'},
+					 label: '2025蛇年导演组'}
 				],
 				// 每个功能主页滚动栏的通用内容
 				noticeBarTextsList: [
